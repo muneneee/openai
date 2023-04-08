f6963729ca30f214158d1d8ea651766f376bc5fa
muneneee <lukekevin39@gmail.com>
2023-04-08 23:30:46 +0300

    intergrated Openai API

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -6,3 +6,37 @@ import { Configuration, OpenAIApi } from 'openai';
 dotenv.config();
 
 const router = express.Router();
+
+const configuration  = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+
+const openai = new OpenAIApi(configuration);
+
+router.route('/').get((req, res) => {
+    res.send('Hello from DALL-E!');
+});
+
+router.route('/').post(async (req, res) => {
+    try {
+        const { prompt } = req.body;
+
+        const aiResponse = await openai.createImage({
+            prompt,
+            n: 1,
+            size: '1024x1024',
+            reaponse_format: 'bs4_json',
+        });
+
+        const image = aiResponse.data.data[0].b64_json;
+
+        res.statusCode(200).json({ photo: image });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error?.response.data.error.message)
+    }
+})
+
+
+export default router;
