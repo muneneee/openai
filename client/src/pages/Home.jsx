2c02bee3760a7d77ab1e7d5a89eb4e369b0868ec
muneneee <lukekevin39@gmail.com>
2023-04-08 20:01:59 +0300

    Added CreatePost

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -22,13 +22,15 @@ const Home = () => {
 
   return (
     <section className='max-w-7xl mx-auto'>
-      <div>
-        <h1 className='font-extrabold text-[#222328] text-[32px]'>
-          The Community Showcase
-        </h1>
-        <p className='mt-2 text-[#666e75] text-[16px] max w [500px]'>
-          Browse through a collection of imaginative and visually stunning impages created by DALL-E AI
-        </p>
+        <div>
+          <h1 className='font-extrabold text-[#222328] text-[32px]'>
+            The Community Showcase
+          </h1>
+          <p className='mt-2 text-[#666e75] text-[16px] max w [500px]'>
+            Browse through a collection of imaginative and visually stunning impages created by DALL-E AI
+          </p>
+        </div>
+
         <div className='mt-16'>
           <FormField />
         </div>
@@ -61,7 +63,6 @@ const Home = () => {
             </>
           )}
         </div>
-      </div>
     </section>
   )
 }
