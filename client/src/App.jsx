66b9a9e5805ae22dfea720f18014a187efcbc1d0
muneneee <lukekevin39@gmail.com>
2023-04-08 18:01:30 +0300

    Added logo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
 import { logo } from './assets';
-// import { Home, Login, Register } from './pages';
+import { Home, CreatePost } from './pages';
 
 const App = () => {
   return (
-    <h1 className='text-3xl font-bold underline'>
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
+        <Link to='/'>
+          <img src={logo} alt='logo' className='w-28 object-contain' />
+        </Link>
+      </header>
+    </BrowserRouter>
   )
 }
 
